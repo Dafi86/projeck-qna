2733b9a0a6ce28034b2179d911eeaac12534f6a5
Al farabi <al.farabi.s@zohomail.com>
2025-12-07 18:48:58 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['user_id'])) {
-    header('Location: controller/dashboard_controller.php');
-} else {
-    header('Location: view/login.php');
-}
-exit;
-?>
